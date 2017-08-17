55c9dd830b7ccf9c992d33a2dafaf8e1346dd4bf
Troy Lamerton <troy.lamerton@gmail.com>
2017-08-17 10:30:30 +0200

    search twitter and log tweets

--- twitter-city-data/config.js
+++ twitter-city-data/config.js
@@ -2,3 +2,16 @@ export const location = {
   city: 'berlin',
   country: 'germany',
 }
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import Twitter from 'twitter'
+
+export const twitter = new Twitter({
+  consumer_key: process.env.TWITTER_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  // bearer_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
--- twitter-city-data/index.js
+++ twitter-city-data/index.js
@@ -1,2 +1,19 @@
-// lets get one tweet from berlin
+import _ from 'lodash'
+import { writeFileSync } from 'fs'
+import path from 'path'
 
+import { twitter as tw } from './config'
+
+tw.get('search/tweets', {q: 'I just liked'})
+  .then((tweets) => {
+    const tweetjson = JSON.stringify(tweets);
+    writeFileSync(path.join(__dirname, 'tweetsdata.json'), tweetjson);
+    const {statuses} = tweets;
+    _.forEach(statuses, (status, i) => {
+      console.log(i, status.text);
+    });
+    console.log(`Total: ${_.size(statuses)} tweets`);
+  })
+  .catch((err) => {
+    console.error(err)
+  })
